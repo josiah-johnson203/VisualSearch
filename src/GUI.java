8964dc69b7a607be4e19e1babe0192f515facd1e
Josiah Johnson <josiahjohnson8120@gmail.com>
2024-11-16 13:56:48 -0500

    switch

--- src/GUI.java
+++ src/GUI.java
@@ -1,7 +1,11 @@
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -10,11 +14,11 @@ import javafx.stage.Stage;
 
 public class GUI extends Application
 {
-	private final int WIDTH = 750;
-	private final int HEIGHT = 500;
+	private final int WIDTH;
+	private final int HEIGHT;
 	
-	private final int PANE_X = (int)0.6*WIDTH;
-	private final int PANE_Y = HEIGHT;
+	private final int PANE_X;
+	private final int PANE_Y ;
 	
 	
 	private Pane pane;
@@ -23,17 +27,33 @@ public class GUI extends Application
 	
 	private Maze maze;
 	
+	/*
+	 * default empty constructor
+	 */
 	public GUI()
 	{
-
+		WIDTH = 750;
+		HEIGHT = 500;
+		
+		PANE_X = (int)0.6*WIDTH;
+		PANE_Y = HEIGHT;
+	}
+	
+	public GUI(int width, int height)
+	{
+		WIDTH = width;
+		HEIGHT = height;
+		
+		PANE_X = (int)0.6*WIDTH;
+		PANE_Y = HEIGHT;
 	}
 	
 	private void initializeGUI(Stage primary)
 	{
-		HBox root = new HBox();
+		BorderPane root = new BorderPane();
 		
 		pane = new Pane();
-		pane.setPrefSize(PANE_X, PANE_Y);
+		pane.setMinSize(PANE_X, PANE_Y);
 		
 		Line line = new Line();
 		
@@ -46,7 +66,11 @@ public class GUI extends Application
 		pane.getChildren().add(line);
 		
 		
+		// -------------- BUTTON VBOX --------------
 		VBox buttons = new VBox();
+		buttons.setAlignment(Pos.TOP_CENTER);
+		buttons.setPadding(new Insets(15));
+		buttons.setSpacing(15);
 		
 		Button btnReset = new Button("Reset Maze");
 		Button btnImport = new Button("Import Maze");
@@ -57,12 +81,13 @@ public class GUI extends Application
 		buttons.getChildren().addAll(btnReset, btnImport, btnFindPath, btnQuit);
 		
 		
-		
+		// -------------- BUTTON LOGIC --------------
 		btnQuit.setOnAction(event -> {
 			primary.close();
 		});
 		
-		root.getChildren().addAll(pane, buttons);
+		root.setCenter(pane);
+		root.setRight(buttons);
 		
 		scene = new Scene(root, WIDTH, HEIGHT);
 		
