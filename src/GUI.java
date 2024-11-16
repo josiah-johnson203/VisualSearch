fa83ca6de690ee9797eb95d96326276a25e06cdb
josiah <josiahjohnson8120@gmail.com>
2024-11-15 21:49:27 -0500

    switch

--- src/GUI.java
+++ src/GUI.java
@@ -1,17 +1,46 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class GUI extends Application
 {
+	private final int WIDTH = 450;
+	private final int HEIGHT = 450;
+	
+	private Canvas canvas;
+	
+	private Scene scene;
+	
+	private Maze maze;
+	
 	public GUI()
 	{
+		initializeGUI();
+	}
+	
+	private void initializeGUI()
+	{
+		VBox buttons = new VBox();
+		
+		Button btnReset = new Button("Reset Maze");
+		Button btnImport = new Button("Import Maze");
+		Button btnFindPath = new Button("Find Path");
+		
+		Button btnQuit = new Button("Quit");
 		
 	}
+	
 	@Override
-	public void start(Stage arg0) throws Exception 
-	{
+	public void start(Stage primary) throws Exception 
+	{	
+		primary.setTitle("Visual Search Algorithm");
+		primary.setScene(scene);
 		
-		
+		primary.show();
 	}
 	
 }
--- src/VisualSearch.java
+++ src/VisualSearch.java
@@ -2,7 +2,10 @@ import javafx.application.Application;
 
 public class VisualSearch 
 {
-
+	
+	/*
+	 * runs the launch method in the GUI class
+	 */
 	public static void main(String[] args) 
 	{
 		Application.launch(GUI.class, args);
